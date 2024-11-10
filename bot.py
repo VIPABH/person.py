9c0698ebaa724601aef44d70e4a981c727945b25
VIPABH <a891983.2@gmail.com>
2024-11-10 03:15:00 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,9 +3,9 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import time
 
 bot = telebot.TeleBot('8191049181:AAGpqs9BQ_BJVa8oeNhsvPFDNxbfcc9BtrI')
-@bot.message_handler(func=lambda message: message.text is not None and 'العربية' in message.text)
-def handle_arabic_message(message):
-    bot.reply_to(message, "اهلا , استخدم `ابدا` او `الشخصيات` لبدء تشغيل البوت ")
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.reply_to(message, "اهلا, استخدم `ابدا` أو `الشخصيات` لبدء تشغيل البوت", parse_mode='Markdown')
 
 @bot.message_handler(func=lambda message: message.text == "الشخصيات" or  message.text == "ابدا")
 def send_welcome(message):
